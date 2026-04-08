f77b2084d5395a6866d0a2c7a865eea64ec130d5
Hugo Averty <hugoaverty@gmail.com>
2026-04-08 10:48:21 +0200

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -2,35 +2,41 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.info('Script started successfully');
-
-let currentPopup: any = undefined;
+console.log('Script started successfully');
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.info('Scripting API ready');
-    console.info('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    WA.room.onEnterLayer("zoneOffice").subscribe(() => {
+        WA.room.hideLayer("Roof/roofOpenOffice");
+        WA.room.hideLayer("Roof/roofOpenOffice2");
+    });
+    WA.room.onLeaveLayer("zoneOffice").subscribe(() => {
+        WA.room.showLayer("Roof/roofOpenOffice");
+        WA.room.showLayer("Roof/roofOpenOffice2");
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onEnter("roofZoneWorkShop").subscribe(() => {
+        WA.room.hideLayer("Roof/roofWorkshop");
+    });
+    WA.room.area.onLeave("roofZoneWorkShop").subscribe(() => {
+        WA.room.showLayer("Roof/roofWorkshop");
+    });
+
+    WA.room.area.onEnter("roofZoneAuditorium").subscribe(() => {
+        WA.room.hideLayer("Roof/roofAuditorium");
+        WA.room.hideLayer("Roof/roofAuditorium2");
+    });
+    WA.room.area.onLeave("roofZoneAuditorium").subscribe(() => {
+        WA.room.showLayer("Roof/roofAuditorium");
+        WA.room.showLayer("Roof/roofAuditorium2");
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
-        console.info('Scripting API Extra ready');
+        console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
